d1fb394d91d07a4d544c0f2b5c92d088c484debd
nicofilliol <nicolas.filliol@outlook.com>
2019-08-23 12:08:59 +0100

    save robot's position to file

--- src/map_saver.cpp
+++ src/map_saver.cpp
@@ -1,33 +1,57 @@
 /**
  * @file map_saver.cpp
  * @author your name (you@domain.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2019-08-22
- * 
+ *
  * @copyright Copyright (c) 2019
- * 
+ *
  */
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "ros/ros.h"
 #include <ros/console.h>
+#include <nav_msgs/Odometry.h>
 
 class MapSaver {
 
 private:
 	ros::NodeHandle n;
+	ros::Subscriber odom_sub;
+
+	float robot_x, robot_y;
 
 	int loop_counter;
 
 	void saveMap() {
-		/* Runs map_saver node in map_server package to save 
+		/* Runs map_saver node in map_server package to save
 		*  occupancy grid from /map topic as image */
 
 		// Save as map_backup.pgm
 		system("roscd robotcraft_maze && rosrun map_server map_saver -f map_backup");
-		ROS_WARN_STREAM("Saving image of map as map_backup.pgm in " << "~/catkin_ws/src/robotcraft_maze/scans");
+	}
 
+	void saveRobotPose() {
+		/* Saves the robot's latest pose which can be used
+		*  for target position calculation */
+		const char* homeDir = getenv("HOME");
+		std::string file(homeDir);
+		file.append("/catkin_ws/src/robotcraft_maze/scans/robot_position.txt");
+
+		std::ofstream position_file;
+		position_file.open (file);
+		position_file << this->robot_x << "\n" << this->robot_y << "\n";
+		position_file.close();
+
+	}
+
+	void odomCallback(const nav_msgs::Odometry::ConstPtr &msg)
+	{
+    this->robot_x = msg->pose.pose.position.x;
+    this->robot_y = msg->pose.pose.position.y;
 	}
 
 public:
@@ -36,6 +60,9 @@ public:
         // Initialize ROS
         this->n = ros::NodeHandle();
 
+				// Add odom subscriber
+				this->odom_sub = this->n.subscribe("odom", 5, &MapSaver::odomCallback, this);
+
     }
 
     void run(){
@@ -43,13 +70,20 @@ public:
         ros::Rate loop_rate(10);
         while (ros::ok())
         {
-        	// Increase loop counter
-        	loop_counter++;	
+        		// Increase loop counter
+        		loop_counter++;
 
-        	// Save map every 100 loops
-        	if(loop_counter % 100 == 0) {
-        	saveMap();
-        	}
+	        	// Save map every 100 loops
+	        	if(loop_counter % 100 == 0) {
+	        	saveMap();
+						saveRobotPose();
+						ROS_WARN_STREAM("Saving image of map as map_backup.pgm"
+						<< " and saving robot's pose as robot_position.txt"
+					 	<< " in ~/catkin_ws/src/robotcraft_maze/scans");
+	        	}
+
+						// Receive messages
+						ros::spinOnce();
 
             // And throttle the loop
             loop_rate.sleep();
